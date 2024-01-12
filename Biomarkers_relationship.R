8dd03b065e5f08ac277036285cf0c6edf859a417
viskky <140535561+viskky@users.noreply.github.com>
2024-01-12 18:52:13 +0100

    Relationship btw Kallistatin & other biomakers

---
+++ Biomarkers_relationship.R
@@ -0,0 +1,7 @@
+
+#Correlation coefficients
+cor.test(df$Kallistatin.D1.,df$CRP,method='spearman')
+cor.test(df$Kallistatin.D1.,df$TNF...D1.,method='spearman')
+cor.test(df$Kallistatin.D1.,df$IL.1..D1.,method='spearman')
+cor.test(df$Kallistatin.D1.,df$IL.6.D1.,method='spearman')
+cor.test(df$Kallistatin.D1.,df$IL.8.D1.,method='spearman')
